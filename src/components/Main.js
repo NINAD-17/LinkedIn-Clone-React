b3573f924f6d34f6daceb614c2daba292888a549
Ninad <ninadkdhulap17@outlook.com>
2023-08-14 16:24:36 +0530

    add code for leftSide and RightSide of homepage section

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Main(props) {
+  return (
+    <Container>Main</Container>
+  )
+}
+
+const Container = styled.div`
+    grid-area: main;
+`;
+
+export default Main
